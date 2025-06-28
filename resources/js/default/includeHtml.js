f5310ca4167e0420b17dce98eaaeb874971ccdc6
potato93 <31458110+Gabeujin@users.noreply.github.com>
2025-06-28 20:30:46 +0900

    Redesign main page app loading

--- resources/js/default/includeHtml.js
+++ resources/js/default/includeHtml.js
@@ -38,60 +38,29 @@ let includeHTML = ()=>{
     }
 }
 
-let includeSection = (i,j)=>{
-  let z, file, xhttp, docXml, docFrag, fileNum;
-  const doNotRead = "0,1,2,3";
-  z = document.querySelector("body>section>contents");
-    do{
-      file = i;
-      if (file) {
-        xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-          if (this.readyState == 4) {
-            if (this.status == 200) {
-              docXml = this.responseXML;
-              docFrag = document.createDocumentFragment();
-              fileNum = docXml.baseURI.split("contents/")[1].replace(".xml","").replace("html","");
-              //check Apps
-              if(SHOW_APP.hasOwnProperty(fileNum)){
-                docFrag.appendChild( getFeature.thumbnail( docXml.querySelector("metaInfo"), docXml.querySelector("link") , fileNum ) );
-                z.appendChild(docFrag);
-                z.appendChild(document.createElement("hr"));
-              }
-            }
-            else if (this.status == 403) {
-              return false;
-            }
-            else if (this.status == 404) {
-              return false;
-            }
-            else if (this.status == 405) {
-              return false;
-            }
-            else if (this.status == 500) {
-              return false;
-            }
-            //last content number;
-            if(i == LAST_PAGE) return false;
+const includeSection = async () => {
+  const container = document.querySelector("body>section>contents");
+  container.innerHTML = "";
+  container.classList.add("app-grid");
 
-            includeSection(i+1, j);
-          }
-        }
-        //last content + 1;
-        if(i == (LAST_PAGE + 1)) return false;
-        file += ".xml";
-        
-        try {
-            xhttp.open("GET", "/views/contents/" + file, true);
-            xhttp.send();
-        } catch (e) {
-          console.log(e);
-        }
-        
-        return false;
-      }
-    }while(true);
-}
+  try {
+    const ids = Object.keys(SHOW_APP);
+    const responses = await Promise.all(
+      ids.map(id => fetch(`/views/contents/${id}.xml`))
+    );
+
+    for (const res of responses) {
+      if(!res.ok) continue;
+      const text = await res.text();
+      const xml = new DOMParser().parseFromString(text, "application/xml");
+      container.appendChild(
+        getFeature.appCard(xml.querySelector("metaInfo"), xml.querySelector("link"))
+      );
+    }
+  } catch(e) {
+    console.log(e);
+  }
+};
 
 let includePage = (file)=>{
   let z, elmnt, xhttp;
